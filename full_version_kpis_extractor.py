e69bc470d2ffa2465b85351b7dccab60833c86f8
KIHALAya <kihalaya00@gmail.com>
2025-04-26 12:27:23 +0100

    successfully extracting counters from formula and investigating suffixes existence

--- full_version_kpis_extractor.py
+++ full_version_kpis_extractor.py
@@ -1,8 +1,8 @@
 import csv
 import re
+import json
 import ast
 import pprint
-import json
 from  collections import defaultdict
 
 """
@@ -18,11 +18,15 @@ EQUIPEMENT_TYPE_MAP = {
     "MGW 15 minutes": "MGW_KPI"
 }
 
-class CounterExtractor(ast.NodeVisitor):
+class KPIProcessor(ast.NodeVisitor):
     """Subclass ast.nodeVistor to define
     what happens when we visit different parts of the tree."""
     def __init__(self):
-        self.counters = {}
+        self.variables = set()
+
+    def visit_Name(self, node):
+        self.variables.add(node.id)
+        self.generic_visit(node)
 
     def infer_type(self, equipement):
         for key, val in EQUIPEMENT_TYPE_MAP.items():
@@ -30,38 +34,15 @@ class CounterExtractor(ast.NodeVisitor):
                 return val
         return "UNKNOWN"
 
-    def visit_Name(self, node):
-        #override the visit_Name which is called every time we find a variable
-        if node.id not in self.counters:
-            self.counters[node.id] = {"has_suffix": False}
-
-    def visit_Attribute(self, node):
-        full_attr = self._get_full_attr(node)
-        root, * suffix = full_attr.split('.')
-        suffix_str = '_'.join(suffix) if suffix else None
-        if root not in self.counters:
-            self.counters[root] = {"has_suffix": False}
-        if suffix:
-            self.counters[root]["has_suffix": True]
-        
-        self.generic_visit(node)
-
-    def _get_full_attr(self, node):
-        if isinstance(node.value, ast.Name):
-            return f"{node.value.id}.{node.attr}"
-        elif isinstance(node.value, ast.Attribute):
-            return f"{self._get_full_attr(node.value)}.{node.attr}"
-        else:
-            return node.attr
-
-    def transform_formula(self,raw_formula, keep_suffix=False, valid_expression=False):
+    def transform_formula(self,raw_formula, keep_suffix=False):
 
         cleaned = raw_formula
 
-        if not keep_suffix:
-            cleaned = re.sub(r'\.\[[^\]]*\]','',cleaned)
-        elif valid_expression:
+        if  keep_suffix:
             cleaned = re.sub(r'\.\[([^\]]+)\]',lambda i: '_' + i.group(1).replace('.','_'),cleaned )
+        else :
+            cleaned = re.sub(r'\.\[[^\]]*\]','',cleaned)
+            
 
         operator_map = {
             r'\{\+\}' : '+',
@@ -81,15 +62,33 @@ class CounterExtractor(ast.NodeVisitor):
     
 
     def extract_counters(self, formula):
+        self.variables.clear()
+
         try:
             tree = ast.parse(formula, mode='eval')
-            extractor = CounterExtractor()
-            extractor.visit(tree)
-            return extractor.counters.copy()
+            self.visit(tree)
         except Exception as e:
             print(f"Error parsing formula: {formula}. Error: {e}")
             return {}
         
+        # pattern = r'\b([^_()\s]+)(?:_([^()\s]+))?\b'
+        # matches = re.findall(pattern, formula)
+
+        counters = {}
+
+        for var in self.variables:
+            parts = var.split('_',1)
+            counter = parts[0]
+            has_suffix = len(parts) > 1
+
+            if counter not in counters:
+                counters[counter] = {"has_suffix": has_suffix}
+
+        return counters  
+
+
+
+
     def build_kpi_dict(self, csv_path):
         kpi_dict = defaultdict(list)
 
@@ -99,10 +98,11 @@ class CounterExtractor(ast.NodeVisitor):
             for row in reader:
                 kpi_name = row['KPI']
                 equipement = row["Equipement"]
-                kpi_type = self.infer_type(equipement)
                 raw_formula = row["Formule"]
 
-                formula = self.transform_formula(raw_formula, keep_suffix=True, valid_expression=True)
+                kpi_type = self.infer_type(equipement)
+
+                formula = self.transform_formula(raw_formula, keep_suffix=True)
                 counters = self.extract_counters(formula)
                 
                 kpi_dict[kpi_name] = {
@@ -114,20 +114,18 @@ class CounterExtractor(ast.NodeVisitor):
         return dict(kpi_dict)
 
 
-
 if __name__ == "__main__":
     csv_path = 'MGW_5_15.csv'
-    extractor = CounterExtractor()
-    kpi_dict = extractor.build_kpi_dict(csv_path)
+    kpi_processor = KPIProcessor()
+    kpi_dict = kpi_processor.build_kpi_dict(csv_path)
+    
 
-    print(json.dumps(kpi_dict, indent=2, ensure_ascii=False))
+    try:
 
-    """try:
+        with open("kpis_data.json", "w", encoding='utf-8') as f:            
+            json.dump(kpi_dict, f, indent=4)
 
-        with open("full_kpis_doc.py", "w", encoding='utf-8') as f:
-            f.write("\n\n# This file is auto-generated using the counters_extractor.py script.\n")
-            f.write("KPI Counters \\\n")
-            pprint.pprint(kpi_dict, stream=f, width=120)
-            print("KPI Counters written to utils.py")
     except Exception as e:
-        print(f"Error writing to file: {e}")"""
+        print(f"Error writing to file: {e}")
+
+    print("kpis data json file created successfully")
